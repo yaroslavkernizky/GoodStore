dfed49915edfeb446a71470f6dc84c0ade948a53
yaroslav <yarius1979@gmail.com>
2015-12-16 16:22:55 +0200

    commit

---
+++ src/js/myJavaScript.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+
+    $("#my-carousel").hover(function () {
+        $(".myCarousel-control").attr('style', 'display:block;');
+
+    }, function () {
+        $(".myCarousel-control").attr('style', 'display:none;');
+
+    });
+
+    $(".opacity").hover(function () {
+        $(this).attr('style', 'z-index:0;');
+    }, function () {});
+
+    $(".my_arrow").hover(function () {}, function () {
+        $(".opacity").attr('style', 'z-index:2;');
+    });
+
+    $("ul.categories li").hover(function () {
+        $(this).attr('style', 'color: #dd1515;');
+        $(this).children().attr('style', 'color: #dd1515;');
+
+    }, function () {
+        $(this).attr('style', 'color: #c1c2c3;');
+        $(this).children("span").attr('style', 'color: #000000;');
+        $(this).children("a").attr('style', 'color: #5e605f;');
+
+    });
+});
